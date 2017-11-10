2f1bc28025277586f9a81fb491911925376bea31
John Knotts <jhnnyk@gmail.com>
2017-11-10 04:30:00 -0700

    default props lesson

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,39 +1,39 @@
 class Counter extends React.Component {
-  constructor (props) {
-    super(props)
-    this.handleAddOne = this.handleAddOne.bind(this)
-    this.handleMinusOne = this.handleMinusOne.bind(this)
-    this.handleReset = this.handleReset.bind(this)
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: 0
-    }
+      count: props.count
+    };
   }
 
-  handleAddOne () {
-    this.setState((prevState) => {
+  handleAddOne() {
+    this.setState(prevState => {
       return {
         count: prevState.count + 1
-      }
-    })
+      };
+    });
   }
 
-  handleMinusOne () {
-    this.setState((prevState) => {
+  handleMinusOne() {
+    this.setState(prevState => {
       return {
         count: prevState.count - 1
-      }
-    })
+      };
+    });
   }
 
-  handleReset () {
+  handleReset() {
     this.setState(() => {
       return {
         count: 0
-      }
-    })
+      };
+    });
   }
 
-  render () {
+  render() {
     return (
       <div>
         <h1>Count: {this.state.count}</h1>
@@ -41,11 +41,15 @@ class Counter extends React.Component {
         <button onClick={this.handleMinusOne}>-1</button>
         <button onClick={this.handleReset}>reset</button>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<Counter />, document.getElementById('app'))
+Counter.defaultProps = {
+  count: 0
+};
+
+ReactDOM.render(<Counter count={4} />, document.getElementById("app"));
 
 // -----------------------------------------------------------------
 // ---------- first version (pre-Component lessons) below ----------
