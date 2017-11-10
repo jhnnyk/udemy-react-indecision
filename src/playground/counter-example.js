998eb9a2a304b7e3d0b4c63895bebdde3989f759
John Knotts <jhnnyk@gmail.com>
2017-11-10 11:39:27 -0700

    add local storage to the counter app

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -9,6 +9,24 @@ class Counter extends React.Component {
     };
   }
 
+  componentDidMount() {
+    try {
+      const count = parseInt(localStorage.getItem("count"), 10);
+
+      if (!isNaN(count)) {
+        this.setState(() => ({ count }));
+      }
+    } catch (error) {
+      // Do nothing
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem("count", this.state.count);
+    }
+  }
+
   handleAddOne() {
     this.setState(prevState => {
       return {
