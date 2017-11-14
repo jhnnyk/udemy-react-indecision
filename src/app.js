35d36d3c8e55728bf2651e3672162f5abaf6a321
John Knotts <jhnnyk@gmail.com>
2017-11-14 08:24:27 -0700

    importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
-import subtract, { square, add } from "./utils.js";
-import isSenior, { isAdult, canDrink } from "./person.js";
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log("app.js is running!");
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 81));
+const template = React.createElement("p", {}, "testing 123");
+ReactDOM.render(template, document.getElementById("app"));
 
-console.log(`is adult at 19: ${isAdult(19)}`);
-console.log(`is adult at 17: ${isAdult(17)}`);
+// import validator from "validator";
+// console.log(validator.isEmail("test@email.com"));
 
-console.log(`can drink at 21: ${canDrink(21)}`);
-console.log(`can drink at 17: ${canDrink(17)}`);
+//-------------------
 
-console.log(`is senior at 65: ${isSenior(65)}`);
-console.log(`is senior at 64: ${isSenior(64)}`);
+// import subtract, { square, add } from "./utils.js";
+// import isSenior, { isAdult, canDrink } from "./person.js";
+
+// console.log("app.js is running!");
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(subtract(100, 81));
+
+// console.log(`is adult at 19: ${isAdult(19)}`);
+// console.log(`is adult at 17: ${isAdult(17)}`);
+
+// console.log(`can drink at 21: ${canDrink(21)}`);
+// console.log(`can drink at 17: ${canDrink(17)}`);
+
+// console.log(`is senior at 65: ${isSenior(65)}`);
+// console.log(`is senior at 64: ${isSenior(64)}`);
